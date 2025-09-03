56c69ffc18b50fc3132dd7424e6b83f95f7248c6
chasshas <gangsolmin7@gmail.com>
2025-09-04 02:17:39 +0900

    interchange

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ height = 720
 fps = 60
 
 #font
+def get_font(size, style: str = "default" ):
+    if style == "default":
+        return pygame.font.Font("assets/fonts/NanumGothic.ttf", size)
+    return pygame.font.Font(f"assets/fonts/NanumGothic{style}.ttf", size)
 font_default = pygame.font.Font("assets/fonts/NanumGothic.ttf", 20)
 #colors
